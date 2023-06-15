ef339b5bc92f6fcfe9fcbbf7f2d71e2c68f08a5e
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-15 21:17:52 +0300

    Создали компонету DisplayLocations, в которой с помощью useQuery(GET_LOCATIONS); получили const { loading, error, data }. Обработали возможные ошибки и замапили дата. Отрисовали DisplayLocations  в Арр.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function App() {
 	return (
 		<div>
 			<h2>My first Apollo app 🚀</h2>
+			<br />
+			<DisplayLocations />
 		</div>
 	);
 }
@@ -19,3 +21,21 @@ const GET_LOCATIONS = gql`
     }
   }
 `;
+
+function DisplayLocations() {
+	const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error : {error.message}</p>;
+
+	return data.locations.map(({ id, name, description, photo }) => (
+		<div key={id}>
+			<h3>{name}</h3>
+			<img width="400" height="250" alt="location-reference" src={`${photo}`} />
+			<br />
+			<b>About this location:</b>
+			<p>{description}</p>
+			<br />
+		</div>
+	));
+}
