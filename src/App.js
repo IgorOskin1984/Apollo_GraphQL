a6298fcf6909fb478b7f87ec3358bb2699ce662b
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-16 11:36:20 +0300

    Перенес DisplayLocations в отдельную компоненту

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useQuery, gql } from '@apollo/client';
+import { DisplayLocations } from './components/DisplayLocations/DisplayLocations';
 
 export default function App() {
 	return (
@@ -9,33 +9,4 @@ export default function App() {
 			{/*<DisplayLocations />*/}
 		</div>
 	);
-}
-
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
-    }
-  }
-`;
-
-function DisplayLocations() {
-	const { loading, error, data } = useQuery(GET_LOCATIONS);
-
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error : {error.message}</p>;
-
-	return data.locations.map(({ id, name, description, photo }) => (
-		<div key={id}>
-			<h3>{name}</h3>
-			<img width="400" height="250" alt="location-reference" src={`${photo}`} />
-			<br />
-			<b>About this location:</b>
-			<p>{description}</p>
-			<br />
-		</div>
-	));
 }
