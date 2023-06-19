dbc12f00e42f545be022318fd7bc01010b646124
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-19 10:49:38 +0300

    Автообновление Добавили pollInterval: 500 в useQuery

--- src/components/Dogs/Dogs.jsx
+++ src/components/Dogs/Dogs.jsx
@@ -49,12 +49,15 @@ export function Dogs() {
 function DogPhoto({ breed }) {
 	const { loading, error, data } = useQuery(GET_DOG_PHOTO, {
 		variables: { breed },
+		pollInterval: 500,
 	});
 
 	if (loading) return null;
 	if (error) return `Error! ${error}`;
 
 	return (
-		<img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+		<div>
+			<img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+		</div>
 	);
 }
